a03213d51f7eede76ad72fdc3e7bfd0e7e68383f
vek <vesa.kivisto@protacon.com>
2017-10-06 09:49:06 +0300

    Added a new tweet.

--- app/data/tweets.js
+++ app/data/tweets.js
@@ -15,6 +15,10 @@ const tweets = [
   {
     data: "[band] [venue]",
     build: arr => `Catch ${arr[0]} playing at ${arr[1]}!`
+  },
+  {
+    data: "[band] [band] [venue]",
+    build: arr => `Don't miss ${arr[0]} with ${arr[1]} todat at ${arr[2]}!`
   }
 ]
 
