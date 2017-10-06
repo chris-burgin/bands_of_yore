398f995c71c528e004de693703188bef623f73cb
Isânio Moraes <isanio@yahoo.com>
2017-10-06 11:11:55 -0300

    #6 add more tweets

--- app/data/tweets.js
+++ app/data/tweets.js
@@ -15,7 +15,23 @@ const tweets = [
   {
     data: "[band] [venue]",
     build: arr => `Catch ${arr[0]} playing at ${arr[1]}!`
-  }
+  },
+  {
+    data: "[band] [venue]",
+    build: arr => `Get your horse and come to ${arr[0]} at ${arr[1]}.`
+  },
+  {
+    data: "[band]",
+    build: arr => `Don't blink on ${arr[0]} performance tomorrow.`
+  },
+  {
+    data: "[band]",
+    build: arr => `${arr[0]} shall go on stage again.`
+  },
+  {
+    data: "[band] [adjective]",
+    build: arr => `${arr[0]} are completely ${arr[1]}.`
+  },
 ]
 
 module.exports = tweets
