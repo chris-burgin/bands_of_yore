f146a5a9def29294e65944e36e8602323fd1160c
Nicholas Fallon <nickfallonwastaken@gmail.com>
2017-10-11 15:54:12 -0600

    Added verbs

--- app/data/verbs.js
+++ app/data/verbs.js
@@ -13,7 +13,7 @@ const verbs = [
   {value: "broadcast"},
   {value: "calculate"},
   {value: "caution"},
-  {value: "chide"}, 
+  {value: "chide"},
   {value: "claim"},
   {value: "clothe"},
   {value: "damage"},
@@ -98,7 +98,11 @@ const verbs = [
   {value: "assist"},
   {value: "serenade"},
   {value: "inquire"},
-  {value: "conjure"}
+  {value: "conjure"},
+  {value: "maim"},
+  {value: "scowl"},
+  {value: "pray"},
+  {value: "craft"}
 ]
 
 module.exports = verbs
